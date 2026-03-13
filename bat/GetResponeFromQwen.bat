2669880963665a68f4ead18812bac6f03db043a6
zuyi53 <rockmanzuyi@live.cn>
2026-03-13 14:14:14 +0800

    Update GetResponeFromQwen.bat

--- bat/GetResponeFromQwen.bat
+++ bat/GetResponeFromQwen.bat
@@ -2,10 +2,10 @@
 call C:\ProgramData\miniconda3\Scripts\activate.bat C:\ProgramData\miniconda3
 call conda activate ai
 
-python "d:/GithubProject/ViDiC-1K/get_response_qwen3.py" ^
+python "d:/GithubProject/ViDiC-1K/inference/get_response_qwen3.py" ^
     --model_name qwen3-vl-8b-instruct ^
-    --input_json_file "d:/GithubProject/ViDiC-1K/input_videos.json" ^
-    --prompt_file "d:/GithubProject/ViDiC-1K/prompt/prompt_generate.txt" ^
+    --input_json "d:/GithubProject/ViDiC-1K/videos/input_videos.json" ^
+    --prompt_file "d:/GithubProject/ViDiC-1K/prompt/prompt_judge.txt" ^
     --batch_size 2 ^
     --fps 2.0 ^
     --gpu_memory_utilization 0.9
